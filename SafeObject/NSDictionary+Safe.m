9cfd8598ac0533848c181e47cfe793d6b97d24e6
donnely_yang <yangdonglin@eapilive.com>
2019-11-22 14:04:08 +0800

    配置文件

--- SafeObject/NSDictionary+Safe.m
+++ SafeObject/NSDictionary+Safe.m
@@ -15,7 +15,6 @@
     [self SwizzlingMethod:@"initWithObjects:forKeys:count:" systemClassString:@"__NSPlaceholderDictionary" toSafeMethodString:@"initWithObjects_st:forKeys:count:" targetClassString:@"NSDictionary"];
 }
 -(instancetype)initWithObjects_st:(id *)objects forKeys:(id<NSCopying> *)keys count:(NSUInteger)count {
-    NSUInteger rightCount = 0;
     for (NSUInteger i = 0; i < count; i++) {
         if (!(keys[i] && objects[i])) {
             if (!objects[i]) {
@@ -23,7 +22,7 @@
             }
         }
     }
-    self = [self initWithObjects_st:objects forKeys:keys count:rightCount];
+    self = [self initWithObjects_st:objects forKeys:keys count:count];
     return self;
 }
 
